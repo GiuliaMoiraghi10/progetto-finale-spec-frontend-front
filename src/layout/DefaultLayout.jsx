98199e04e52a0721861aac2926b2a412f7d3dc42
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-14 10:03:24 +0200

    Creo componenti Header e Footer e li importo in DefaultLayout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,3 +1,14 @@
-export default function Defaultlayout() {
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
+export default function Defaultlayout() {
+    return (
+        <div>
+            <Header />
+            <main>
+
+            </main>
+            <Footer />
+        </div>
+    )
 }
