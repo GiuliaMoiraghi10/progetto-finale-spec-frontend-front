110c2d170e431b7a0831ebdf8e39173348241dfc
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-14 16:46:00 +0200

    Sistemo customHook e GlobalContext

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,11 +1,17 @@
 import { createContext, useContext } from "react";
+import useSailor from "../hooks/useSailor";
 
 const GlobalContext = createContext();
 
-export function useGlobalContext() {
-    return useContext(GlobalContext)
+export function GlobalProvider({ children }) {
+    // variabile che contiene tutto il return di useSailor()
+    const sailorData = useSailor()
+
+    return (
+        <GlobalContext.Provider value={{ ...sailorData }}>
+            {children}
+        </GlobalContext.Provider>
+    )
 }
 
-export function GlobalProvider({ children }) {
-    return <GlobalContext.Provider>{children}</GlobalContext.Provider>
-}
+export default GlobalContext
--- src/hooks/useSailor.js
+++ src/hooks/useSailor.js
@@ -1,17 +1,26 @@
 import { useState, useEffects } from 'react'
 
-function useSailor() {
+export default function useSailor() {
+    // stato per le Sailor
     const [sailor, setSailor] = useState([])
 
+    // fetchSailor x prendere i dati di tutte le Sailor
     const fetchSailor = async () => {
-        const response = await fetch("http://localhost:3001/sailors")
-        const data = await response.json()
-        setSailor(data)
+        try {
+            const response = await fetch("http://localhost:3001/sailors")
+            const data = await response.json()
+            setSailor(data)
+        }
+        catch (error) {
+            console.error(error)
+        }
+
+        // useEffects x chiamare la funzione fetchSailor() quando il componente viene montato
+        useEffects(() => {
+            fetchSailor()
+        }, [])
     }
 
-    useEffects(() => {
-        fetchSailor
-    }, [])
 
     // getSailor x prendere i dati di una singola Sailor tramite id
     const getSailor = async (id) => {
@@ -20,7 +29,5 @@ function useSailor() {
         return data
     }
 
-    return { sailor, setSailor, getSailor }
+    return { sailor, setSailor, fetchSailor, getSailor }
 }
-
-export default { sailor }
