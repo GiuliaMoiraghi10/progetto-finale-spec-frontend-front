08e42af6f0542ceb2ef14ea5d891d6f83204a0d0
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-14 09:55:50 +0200

    Creo struttura rotte + DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layout/DefaultLayout"
 function App() {
 
   return (
     <>
-      <section className="container">
-        <h1 className="text-2xl">Progetto Finale</h1>
-      </section>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
