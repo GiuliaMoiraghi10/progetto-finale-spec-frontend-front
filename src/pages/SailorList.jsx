94492221ab453b17b4b14dc79edd46f6c67e547e
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-15 12:13:53 +0200

    Aggiungo logica per filtrare Sailor

--- src/pages/SailorList.jsx
+++ src/pages/SailorList.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react'
+import { GlobalProvider } from '../contexts/GlobalContext'
+
 export default function SailorList() {
+
+    const { sailor } = GlobalProvider()
+    const [search, setSearch] = useState('')
+    const [selectCategory, setSelectCategory] = useState('')
+
+    const filteredSailor = () => {
+        return [...sailor].filter((s) => {
+            const query = s.title.toLowerCase().includes(search.toLowerCase().trim())
+            const category = selectCategory === '' || s.category === selectCategory
+            return query && category
+        })
+    }
+
     return (
-        <div>
-            <h1>Lista Sailor</h1>
-        </div>
+        <>
+            <div>
+                <h1>Lista Sailor</h1>
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <select
+                    name="category"
+                    id="category"
+                    onChange={(e) => setSelectCategory(e.target.value)}
+                >
+                    <option value="">Inner Senshi</option>
+                    <option value="">Outer Senshi</option>
+                    <option value="">Alleato</option>
+                    <option value="">Starlights</option>
+                    <option value="">Nemico</option>
+                </select>
+            </div>
+            <button>Ordina per nome</button>
+            <div>
+                {filteredSailor.map((s) => {
+                    <div key={s.id}>{s.title}</div>
+                })}
+            </div>
+        </>
     )
 }
