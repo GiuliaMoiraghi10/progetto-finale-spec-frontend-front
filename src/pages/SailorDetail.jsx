f0eb57a24a368e67aaeb6e5f717ff210438d7555
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-16 16:06:48 +0200

    Aggiungo e implemento componente SailorDetail.jsx

--- src/pages/SailorDetail.jsx
+++ src/pages/SailorDetail.jsx
@@ -1,7 +1,50 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useGlobalContext } from '../contexts/GlobalContext'
+
 export default function SailorDetail() {
+
+    const { id } = useParams()
+    const { getSailor } = useGlobalContext()
+    const [sailor, setSailor] = useState()
+
+    useEffect(() => {
+        async function getSailorData() {
+            try {
+                const data = await getSailor(id)
+                setSailor(data.sailor)
+            } catch (error) {
+                console.error("Errore nel recupero dei dati:", error)
+            }
+        }
+        getSailorData()
+    }, [])
+
+    if (!sailor) {
+        return <p>Caricamento...</p>
+    }
+
+    const { title, category, image, description, planet, solarSystem, transformation, weapons } = sailor
+    console.log("Dettagli Sailor:", sailor)
+
     return (
-        <div>
-            <h1>Dettagli Sailor</h1>
-        </div>
+        <>
+            <div>
+                <h1>Dettagli Sailor</h1>
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <img src={image} alt="" />
+                <p>{category}</p>
+                <p>{description}</p>
+            </div>
+            <div>
+                <h3>Dettagli aggiuntivi:</h3>
+                <p>Pianeta: {planet ? planet : "Nessun dato"}</p>
+                <p>Sistema Solare: {solarSystem ? solarSystem : "Nessun dato"}</p>
+                <p>Trasformazioni: {transformation !== undefined ? transformation : "Nessun dato"}</p>
+                <p>Armi: {weapons ? weapons : "Nessun dato"}</p>
+            </div>
+        </>
     )
 }
