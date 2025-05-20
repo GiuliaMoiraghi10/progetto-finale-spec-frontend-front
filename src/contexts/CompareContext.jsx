2b5d6a02bd0cc8a11e5f12a8f3eeef099c7c8849
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-20 16:52:23 +0200

    Modifica CompareContext

--- src/contexts/CompareContext.jsx
+++ src/contexts/CompareContext.jsx
@@ -2,36 +2,48 @@ import { createContext, useContext } from "react";
 import { useGlobalContext } from "./GlobalContext";
 import useCompare from "../hooks/useCompare";
 
-const CompareContext = createContext()
+const CompareContext = createContext();
 
 export function useCompareContext() {
-    return useContext(CompareContext)
+    return useContext(CompareContext);
 }
 
 export function CompareProvider({ children }) {
-    const { getSailor } = useGlobalContext()
-    const { showCompare, setShowCompare, sailorsToCompare, setSailorsToCompare } = useCompare()
-}
+    const { getSailor } = useGlobalContext();
+    const { showCompare, setShowCompare, sailorsToCompare, setSailorsToCompare } = useCompare();
 
-async function getItem(id) {
-    const data = await getSailor(id)
-    return data.sailor
-}
-
-const compareSailors = async (id) => {
-    if (
-        !sailorsToCompare.some((sailor) => sailor.id === id) &&
-        sailorsToCompare.length < 2
-    ) {
-        const sailor = await getItem(id)
-        setSailorsToCompare((prevSailor) => [...prevSailor, sailor])
-    } else if (sailorsToCompare.some((sailor) => sailor.id === id)) {
-        setSailorsToCompare((prevSailor) =>
-            prevSailor.filter((sailor) => sailor.id !== id))
+    // Funzione per ottenere un sailor
+    async function getItem(id) {
+        const data = await getSailor(id);
+        return data.sailor;
     }
-}
 
-return (
-    <CompareContext.Provider
-        value={{}}>{children}</CompareContext.Provider>
-)
+    // Funzione per aggiungere/rimuovere sailor dal confronto
+    const compareSailors = async (id) => {
+        if (
+            !sailorsToCompare.some((sailor) => sailor.id === id) &&
+            sailorsToCompare.length < 2
+        ) {
+            const sailor = await getItem(id);
+            setSailorsToCompare((prevSailor) => [...prevSailor, sailor]);
+        } else if (sailorsToCompare.some((sailor) => sailor.id === id)) {
+            setSailorsToCompare((prevSailor) =>
+                prevSailor.filter((sailor) => sailor.id !== id)
+            );
+        }
+    };
+
+    return (
+        <CompareContext.Provider
+            value={{
+                showCompare,
+                setShowCompare,
+                sailorsToCompare,
+                setSailorsToCompare,
+                compareSailors,
+            }}
+        >
+            {children}
+        </CompareContext.Provider>
+    );
+}
